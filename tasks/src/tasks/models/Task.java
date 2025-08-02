b1697a6f01e3d545d610f2542de2cf3567257dfa
Hellen-2025 <hellenramirez2025@gmail.com>
2025-08-02 17:42:38 -0500

    Se crea atributos, constructor con parametros, asignaciones, setters, getters

--- tasks/src/tasks/models/Task.java
+++ tasks/src/tasks/models/Task.java
@@ -1,48 +1,43 @@
 package tasks.models;
 
 public class Task { 
-     //Atributos
+    // Atributos
     private String name;
     private String description;
     private boolean status;
 
-   
-//Constructor con parametros
+    // Constructor con parámetros
     public Task(String name, String description, boolean status) {
         this.name = name;
         this.description = description;
         this.status = status;
-
-        // el this hace referencia a los atributos de la clase
-        // en este cado los atributos y los metodos
-
     }
 
-    // Setters -Asignaciones
-
-    public void setName(String name) { //metodo publico paea asignar el valor
-        this.name = name; //Asignación
-         
+    // Setters - Asignaciones
+    public void setName(String name) {
+        this.name = name;
     }
-    
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public void setStatus(boolean status) {
         this.status = status;
     }
-    
-
-
 
     // Getters - Obtener
     public String getName() {
         return name;
+    }
 
     public String getDescription() {
         return description;
+    }
 
     public boolean getStatus() {
         return status;
     }
 }
+
+
